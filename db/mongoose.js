01c7b368c35517ef7cef34583eebbaf84ad57ca8
ctiller15 <christiller2015@gmail.com>
2017-10-02 20:15:24 -0400

    Add mongodb URI, add node engine

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/idea_saver');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/idea_saver');
 
 module.exports = mongoose;
